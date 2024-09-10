9876aa6f114e8cede521574de9dc27ebd40dc487
user <daulykumari7028@gmail.com>
2024-09-11 00:54:24 +0530

    this is service register

---
+++ src/main/java/spring_register/registor/RegistorApplication.java
@@ -0,0 +1,16 @@
+package spring_register.registor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+
+@EnableEurekaServer
+public class RegistorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_register/registor/RegistorApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_register.registor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
